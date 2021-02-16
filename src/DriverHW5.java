0d64845a25f49cad039a15e24fe18ba4ff5e0f11
CUWSulli <78753434+CUWSulli@users.noreply.github.com>
2021-02-16 12:51:11 -0600

    Update DriverHW5.java

--- src/DriverHW5.java
+++ src/DriverHW5.java
@@ -1,4 +1,5 @@
-
+import java.net.URL;
+import java.util.Scanner;
 public class DriverHW5 
 {
 	public static void main(String[] args)
@@ -7,5 +8,31 @@ public class DriverHW5
 		MinecraftMobAttributes Creeper = new MinecraftMobAttributes("Creeper", 20, 22, "Gunpowder");
 		Skeleton.display();
 		Creeper.display();
+	
+		String CardJSON = DriverHW5.getJSON("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+		System.out.println(CardJSON);
+	}
+	
+	static String getJSON(String urlString)	
+	{
+		String line = "";
+		try
+		{
+			URL url = new URL(urlString);
+			Scanner input = new Scanner(url.openStream());
+			while (input.hasNext())
+			{
+				line += input.nextLine();
+			}
+			input.close();
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			line = "Can't Get URL";
+		}
+		return line;
+		 
 	}
 }
